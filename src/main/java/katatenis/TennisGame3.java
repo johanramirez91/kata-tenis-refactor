e364c4cedfc456ee86b76d2af48225a38e6db681
Johan Ramirez <johan911019@gmail.com>
2021-12-14 16:31:49 -0500

    Refactor game3 completed

--- src/main/java/katatenis/TennisGame3.java
+++ src/main/java/katatenis/TennisGame3.java
@@ -1,37 +1,63 @@
 package katatenis;
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int scorePlayer1;
+    private int scorePlayer2;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-        }
-    }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
-        else
-            this.p2 += 1;
-        
+        if (scoresComparing()) return scoreResult();
+        if (isTieBreak()) return "Deuce";
+        return getWinner();
     }
 
+    public void wonPoint(String playerName) {
+        if (playerName.equalsIgnoreCase("player1"))
+            this.scorePlayer1 += 1;
+        if (playerName.equalsIgnoreCase("player2"))
+            this.scorePlayer2 += 1;
+    }
+
+    private boolean isTieBreak() {
+        return scorePlayer1 == scorePlayer2;
+    }
+
+    private boolean isAdvantageOrWin(){
+        return Math.pow((scorePlayer2 - scorePlayer1), 2) == 1;
+    }
+
+    public String getWinner(){
+        return isAdvantageOrWin() ? "Advantage " + playerAdvantage() : "Win for " + playerAdvantage();
+    }
+
+    public String playerAdvantage(){
+        return scorePlayer2 > scorePlayer1 ? player2Name : player1Name;
+    }
+
+    private boolean scoresComparing() {
+        return (isLessThan4(scorePlayer2) && isLessThan4(scorePlayer1) && isDifferentTo6());
+    }
+
+    private boolean isLessThan4(int scorePlayer) {
+        return scorePlayer < 4;
+    }
+
+    private boolean isDifferentTo6() {
+        return (scorePlayer1 + scorePlayer2 != 6);
+    }
+
+    public String scoreResult() {
+        String[] scores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        return (scorePlayer2 == scorePlayer1) ?
+                scores[scorePlayer1] + "-All"
+                :
+                scores[scorePlayer1] + "-" + scores[scorePlayer2];
+    }
 }
